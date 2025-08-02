935f91361ed913db23f8fa646a11f3e80269be26
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-08-02 21:05:02 +0200

    hello world in zig

---
+++ main.zig
@@ -0,0 +1,7 @@
+const std = @import("std"); //const std = @import(std); --> Gives an error package names must be strings
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    try stdout.print("Hello world\nyesssir", .{});
+}
